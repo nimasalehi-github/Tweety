4ee3d8b153a43c7eee008817b050406f990255dd
“nimasalehi-github” <nimasalehi.auth@icloud.com  git config --global user.email nimasalehi.auth@icloud.com>
2020-08-21 15:38:50 +0430

    https://angular.io/guide/http#optimizing-server-interaction-with-debouncing

--- README.md
+++ README.md
@@ -17,4 +17,9 @@ Get started with the Twitter developer platform
             app/uploader/uploader.service.ts (upload request)
             app/uploader/uploader.service.ts (upload body)
             app/uploader/uploader.service.ts (getEventMessage)
+
+        https://angular.io/guide/http#optimizing-server-interaction-with-debouncing
+            app/package-search/package-search.component.html (search)
+            app/package-search/package-search.component.ts (excerpt)
+            https://angular.io/guide/http#using-the-switchmap-operator
             
--- angulario/http/src/app/package-search/package-search.component.ts
+++ angulario/http/src/app/package-search/package-search.component.ts
@@ -1,3 +1,9 @@
+/**
+ *   https://angular.io/guide/http#optimizing-server-interaction-with-debouncing
+  *   app/package-search/package-search.component.html (search)
+  *   app/package-search/package-search.component.ts (excerpt)
+ *    https://angular.io/guide/http#using-the-switchmap-operator
+*/
 import { Component, OnInit } from '@angular/core';
 
 import { Observable, Subject } from 'rxjs';
@@ -11,15 +17,15 @@ import { NpmPackageInfo, PackageSearchService } from './package-search.service';
   providers: [ PackageSearchService ]
 })
 export class PackageSearchComponent implements OnInit {
-  withRefresh = false;
-  packages$: Observable<NpmPackageInfo[]>;
+  withRefresh = false; // https://angular.io/guide/http#optimizing-server-interaction-with-debouncing
+  packages$: Observable<NpmPackageInfo[]>;  //  https://angular.io/guide/http#optimizing-server-interaction-with-debouncing
   private searchText$ = new Subject<string>();
 
-  search(packageName: string) {
+  search(packageName: string) { //    https://angular.io/guide/http#optimizing-server-interaction-with-debouncing
     this.searchText$.next(packageName);
   }
 
-  ngOnInit() {
+  ngOnInit() {  //  https://angular.io/guide/http#optimizing-server-interaction-with-debouncing
     this.packages$ = this.searchText$.pipe(
       debounceTime(500),
       distinctUntilChanged(),
@@ -28,7 +34,7 @@ export class PackageSearchComponent implements OnInit {
     );
   }
 
-  constructor(private searchService: PackageSearchService) { }
+  constructor(private searchService: PackageSearchService) { }  //  https://angular.io/guide/http#optimizing-server-interaction-with-debouncing
 
 
   toggleRefresh() { this.withRefresh = ! this.withRefresh; }
